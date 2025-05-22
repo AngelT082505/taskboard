61b99f5d75e670f181be7019d877c834fe1e5ed3
Angel-Taveras <angeltaveras24@gmail.com>
2025-05-22 14:14:30 +0200

    web config

--- src/main/java/org/angel/taskboard/config/WebConfig.java
+++ src/main/java/org/angel/taskboard/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "http://localhost:5174", "http://localhost:5175")
+                        .allowedOrigins("http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "https://taskboard-angel.netlify.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowCredentials(true);
             }
